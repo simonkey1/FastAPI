653b2224a8d2e5c8c969c4883286f27278d8f904
Simon_linux <cris.linkinday@gmail.com>
2025-05-10 11:11:40 -0400

    crud implemented, put still working wrong

--- users.py
+++ users.py
@@ -54,3 +54,49 @@ def search_user(id: int):
         return list(users)[0]
     except:
         return {"error: no se ha encontrado el usuario"}  
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error: el usuario ya existe"}  
+    
+    users_list.append(user)
+    return user
+
+
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error: no se ha encontrado el usuario. "}  
+
+@app.put("/user/")
+async def user(user: User):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] == user
+            found = True
+
+    if not found:
+        return {'error: no se ha actualizado el usuario. '}
+    
+    return user
+    
+
+@app.delete("/user/{id}")
+async def user(id: int):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+        
+    if not found:
+        return {'error': 'No se ha eliminado el usuario' }
